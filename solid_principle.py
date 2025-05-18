04a430f7abbdab420bf5f9666db77dd7776e117d
Aditya Parashar <parashar.aditya1990@gmail.com>
2025-05-18 18:57:09 +0530

    Introduction to SOLID Principles

---
+++ solid_principle.py
@@ -0,0 +1,11 @@
+# Solid Principles are  5 software design principles that help you to write high quality, fleixible, maintainable, reusable, readable and testable software.
+
+# SOLID Principle was introduced by a software engineer Robert C Martin also known as Uncle Bob in the early 2000, and Uncle Bob aim was to promote good quality software practices. particularly in OOP by addressing common problems devlopers faced as software systems grown insize and complexity.
+
+# S - single Responsibility Principle
+# O - Open-Closed Principle
+# L - Liskov Substitution Principle
+# I -Interface Segregation Principle
+# D - Dependency Inversion Principle
+
+# By following these principles you can create software designs that are easy to understand, maintain and extend, leading to higher quality software that is more robust and adaptable to change 
