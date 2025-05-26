76e6651a7f7cdea0529345fb6cfa2fed2c382d52
Aditya Parashar <parashar.aditya1990@gmail.com>
2025-05-26 11:51:38 +0530

    Intro to design patterns

---
+++ design _patterns.py
@@ -0,0 +1,21 @@
+# What are Design Patterns
+
+# Design patterns are proven, reusable solutions to common problem in software design, providing templates or guidelines for structuring code to resolve recurring challenges in a consistent and efficient way. 
+
+# Why Design Patterns?
+
+# 1. Reusable solutions - Sp design patterns provide proven solutions to recurring problems in software designs. So instead of reinventing the wheel developers can leverage patterns to solve common issues. 
+
+# 2. Standardized Termonology - They also provide a standardized common language for developers to communicate effectively about software designs, and the common vocabulary enhances collaboration and understanding among team members.
+
+# 3. Scalabitlity - Design patterns also promote scalable designs by providing flexible and adaptable solutions, and they allow system to evolve over time without extensive rework and architectural overhaul. 
+
+# 4. Maintainability - Using design patterns often results in more maintainable code as patterns encapsulate design decisions and promote modular, loosely coupled architectures, making it easier to understand, modify and extend code bases
+
+# 5. Performance - Some design patterns help to improve performance by optimising resource usage such as memory in RAM
+
+# 6. Documentation - Design Patterns can also be used as a documentation for software designs 
+
+# 7. Best Practices - They embody best practices and principles of software design as they can encapsulate years of collective knowledge and experience guiding developers toward solutions that are robust, reliable and maintainable.
+
+# 8. Cross Domain Applicability - Many design patterns are agnostic or dont depend on the programming language or domain being used. They can be applied across different technologies and industries, making them very valuable tools for developers working in diverse environments. 
