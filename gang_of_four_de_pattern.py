3cea46fedc7a113ad6dc4a65e3061910949fb739
Aditya Parashar <parashar.aditya1990@gmail.com>
2025-05-26 12:04:18 +0530

    Gang of Four Design Patterns

---
+++ gang_of_four_de_pattern.py
@@ -0,0 +1,17 @@
+# # The Gang of Four Design Patterns - It consists of 23 design patterns. These 23 design patterns can be grouped into three categories 
+
+# 1. Creational - The different ways to create objects
+# 2. Structural - The relationship between those objects
+# 3. Behavioral - The interaction or communication between those objects.
+
+# - Behavioural : 
+#   o State Pattern
+#   o Observer Pattern
+
+# - Structural :
+#   o Facade Pattern
+#   o Adapter Pattern 
+
+# - Creational
+#   o Prototype Pattern
+#   o Abstract Factory Pattern 
